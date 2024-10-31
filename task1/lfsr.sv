abe7bd02f55e280596489ec90237b050cd9d13ea
aa6dcc <142180482+aa6dcc@users.noreply.github.com>
2024-10-31 14:00:14 +0000

    Task 1 Complete

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -1,8 +1,19 @@
-module lfsr(
-    input   logic       clk,
-    input   logic       rst,
-    input   logic       en,
-    output  logic [3:0] data_out
+module lfsr (
+    input logic         clk,
+    input logic         rst,
+    input logic         en,
+    output logic [3:0]  data_out
 );
+
+logic [3:0] sreg;   
+
+always_ff @(posedge clk, posedge rst) begin
+    if (rst)
+        sreg <= 4'b1; 
+    else if (en)
+        sreg <= {sreg[2:0], sreg[3] ^ sreg[2]};    
+end
+
+assign  data_out = sreg;
 
 endmodule
--- task1/lfsr_7.sv
+++ task1/lfsr_7.sv
@@ -1,8 +1,19 @@
 module lfsr_7 (
-    input   logic       clk,
-    input   logic       rst,
-    input   logic       en,
-    output  logic [6:0] data_out
+    input logic         clk,
+    input logic         rst,
+    input logic         en,
+    output logic [6:0]  data_out
 );
+
+logic [6:0] sreg;   //sreg is a 7 bit long representation of the shift registers
+
+always_ff @(posedge clk, posedge rst) begin
+    if (rst)
+        sreg <= 7'b1;   //reset all 1s since all 0 will make it static
+    else if (en)
+        sreg <= {sreg[5:0], sreg[2] ^ sreg[6]};      //XOR 3rd and 7th register, append it as input in LSB
+end
+
+assign  data_out = sreg;
 
 endmodule
